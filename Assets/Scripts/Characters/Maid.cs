b8c04acbbf4f4f9e3787b130d462fb3db0157de3
unknown <sapphon@gmail.com>
2024-06-25 19:36:55 -0400

    Add to citybox
    Also remove redundant Assets folder, oopsie

---
+++ Assets/Scripts/Characters/Maid.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burglar : Character
+{
+    void Awake()
+    {
+        this.advantageDescription = "Unsuspicious when indoors.";
+    }
+}
---
+++ Assets/Scripts/Triggers/EscapeRoute.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeRoute : AbstractTrigger
+{
+    protected override void doEffects()
+    {
+        throw new System.NotImplementedException();
+    }
+}
